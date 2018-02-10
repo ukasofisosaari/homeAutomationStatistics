7c43988e6db2e1d377d72e9f8123a74e5f30150a
Saku Rautiainen <saku.rautiainen@iki.fi>
2018-02-10 20:13:19 +0200

    Cleaning code

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -5,29 +5,30 @@
 
 #!/usr/bin/env python3
 from time import gmtime, strftime
+import socket
 import serial
 import requests
-import socket
 SERIAL_PORT = '/dev/ttyUSB0'
 BAUDRATE = 9600
 URL = 'localhost:3001/api/data'
 N_FIELDS_IN_MSG = 11
 
-def postToWebServer(query):
+def post_to_web_server(query):
+    """ Method for posting to web server """
     query['hostname'] = socket.gethostname()
     print(query)
     try:
         res = requests.post(URL, data=query)
         print(res.text)
-    except requests.exceptions.InvalidSchema as e:
-        print(e)
+    except requests.exceptions.InvalidSchema as request_error:
+        print(request_error)
 
 def main():
     """ Main function """
     try:
         ser = serial.Serial(
             port=SERIAL_PORT,
-            baudrate = 9600,
+            baudrate=9600,
             parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
             bytesize=serial.EIGHTBITS,
@@ -37,9 +38,8 @@ def main():
     except serial.serialutil.SerialException:
         ser = None
         query = {'error_msg' : "No device on serial port: {}".format(SERIAL_PORT),
-        'time' : strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        }
-        postToWebServer(query)
+                 'time' : strftime("%Y-%m-%d %H:%M:%S", gmtime())}
+        post_to_web_server(query)
 
 
     data_array = []
@@ -53,19 +53,19 @@ def main():
         if len(mesh_msg) == N_FIELDS_IN_MSG and mesh_msg[1] == 'R':
 
             sensor_data = {}
-            for i in range(len(mesh_msg)):
-                if mesh_msg[i] == 'R':
+            for i, value in enumerate(mesh_msg):
+                if value == 'R':
                     sensor_data['node_id'] = mesh_msg[i+1]
-                elif mesh_msg[i] == 'H':
+                elif value == 'H':
                     sensor_data['humidity'] = mesh_msg[i+1]
-                elif mesh_msg[i] == 'T':
+                elif value == 'T':
                     sensor_data['temperature'] = mesh_msg[i+1]
             sensor_data['time'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
             data_array.append(sensor_data)
 
         if len(data_array) > 10:
             query = {'data': data_array}
-            postToWebServer(query)
+            post_to_web_server(query)
 
 
 if __name__ == "__main__":
