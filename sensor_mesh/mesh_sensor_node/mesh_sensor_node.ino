eac711330b05ddae6f9d1b07da04e97ffc57e439
Saku Rautiainen <saku.rautiainen@iki.fi>
2018-04-03 17:30:58 +0300

    Cleanup

--- sensor_mesh/mesh_sensor_node/mesh_sensor_node.ino
+++ sensor_mesh/mesh_sensor_node/mesh_sensor_node.ino
@@ -9,13 +9,17 @@
 #include <painlessMesh.h>
 #include <painlessMeshSync.h>
 #include <painlessScheduler.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
-
+#define DHTPIN 4     // what digital pin the DHT22 is conected to
+#define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
 
 #define   MESH_PREFIX     "HakalaSensorNode"
 #define   MESH_PASSWORD   "password"
 #define   MESH_PORT       5555
 
+DHT dht(DHTPIN, DHTTYPE);
 
 enum meshNodeType {
   MESH_GATEWAY = 1,
@@ -24,8 +28,39 @@ enum meshNodeType {
 };
 
 meshNodeType node_type = MESH_UNKNOWN;
+void sendMessage() ;
 painlessMesh  mesh;
+Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
 
+void sendMessage() {
+    String error_msg = "";
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t) ) {
+      error_msg = "Failed to read from sensor, nodeid: ";
+    }
+
+    
+    String msg = ";";
+    if(error_msg.length() == 0) {
+      msg += mesh.getNodeId();
+      msg +=";H;" + String(h) + ";T;" + String(t) + ";\n";
+    }
+    else {
+      msg = mesh.getNodeId();
+      msg += error_msg;
+    }
+  
+    mesh.sendBroadcast( msg );
+    
+    Serial.printf(" Sent message;%s", msg.c_str());
+    taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+}
 
 
 
@@ -55,16 +90,21 @@ void setup() {
   mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
   mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 
-  node_type = MESH_GATEWAY;  
 
+  
   mesh.onReceive(&receivedCallback);
   mesh.onNewConnection(&newConnectionCallback);
   mesh.onChangedConnections(&changedConnectionCallback);
 
+
+  node_type = MESH_DHT_NODE;
+  mesh.scheduler.addTask( taskSendMessage );
+  taskSendMessage.enable() ;
   Serial.printf("Setup Done");
 
 }
 
 void loop() {
   mesh.update();
+  //Serial.printf("Setup Done");
 }
