600860478e2aaec0b723e5c7165e80098c43df15
Saku Rautiainen <saku.rautiainen@iki.fi>
2018-04-08 19:35:38 +0300

    should work

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -3,21 +3,21 @@ var util = require("util");
 const fs = require("fs");
 const jsonConfig = JSON.parse(fs.readFileSync("./config/config.json"));
 
-var url = util.format("mongodb://%s:%s@%s:%s/%s?authSource=%s",
+var url = util.format("mongodb://%s:%s@%s:%s/%s",
     jsonConfig.mongodb_username,
     jsonConfig.mongodb_pwd,
     jsonConfig.mongodb_url,
     jsonConfig.mongodb_port,
-    jsonConfig.mongodb_database,
     jsonConfig.mongodb_database
 );
 
 
 exports.mesh_data_packet = function (req, res) {
     console.log("Inbound data packet");
-    mongoClient.connect(url, function (err, db) {
+    MongoClient.connect(url, function (err, db) {
         if (err) throw err;
-        db.collection(
+        var dbo = db.db(jsonConfig.mongodb_database);
+        dbo.collection(
             jsonConfig.mongodb_collection_data).insertOne(req.body, function(err, res) {
             if (err) throw err;
             console.log("1 document inserted");
