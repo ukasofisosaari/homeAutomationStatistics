4dde30ae0f7b8fae64b1865742a1c20440855620
Saku Rautiainen <saku.rautiainen@iki.fi>
2018-04-08 18:59:43 +0300

    Back to previous version

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -3,12 +3,11 @@ var util = require("util");
 const fs = require("fs");
 const jsonConfig = JSON.parse(fs.readFileSync("./config/config.json"));
 
-var url = util.format("mongodb://%s:%s@%s:%s/%s",
+var url = util.format("mongodb://%s:%s@%s:%s",
     jsonConfig.mongodb_username,
     jsonConfig.mongodb_pwd,
     jsonConfig.mongodb_url,
-    jsonConfig.mongodb_port,
-    jsonConfig.mongodb_database
+    jsonConfig.mongodb_port
 );
 
 
@@ -16,7 +15,9 @@ exports.mesh_data_packet = function (req, res) {
     console.log("Inbound data packet");
     mongoClient.connect(url, function (err, db) {
         if (err) throw err;
-        db.collection(
+        var dbo = db.db(jsonConfig.mongodb_database);
+        var myData = {  };
+        dbo.collection(
             jsonConfig.mongodb_collection_data).insertOne(req.body, function(err, res) {
             if (err) throw err;
             console.log("1 document inserted");
