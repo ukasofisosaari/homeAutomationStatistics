64f07a1e3cbd607392937ff1e95cfd18e9fff7d8
Saku Rautiainen <saku.rautiainen@iki.fi>
2018-04-08 19:25:23 +0300

    Straight out connecting to db

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -3,11 +3,13 @@ var util = require("util");
 const fs = require("fs");
 const jsonConfig = JSON.parse(fs.readFileSync("./config/config.json"));
 
-var url = util.format("mongodb://%s:%s@%s:%s",
+var url = util.format("mongodb://%s:%s@%s:%s/%s?authSource=%s",
     jsonConfig.mongodb_username,
     jsonConfig.mongodb_pwd,
     jsonConfig.mongodb_url,
-    jsonConfig.mongodb_port
+    jsonConfig.mongodb_port,
+    jsonConfig.mongodb_database,
+    jsonConfig.mongodb_database
 );
 
 
@@ -15,9 +17,7 @@ exports.mesh_data_packet = function (req, res) {
     console.log("Inbound data packet");
     mongoClient.connect(url, function (err, db) {
         if (err) throw err;
-        var dbo = db.db(jsonConfig.mongodb_database);
-        var myData = {  };
-        dbo.collection(
+        db.collection(
             jsonConfig.mongodb_collection_data).insertOne(req.body, function(err, res) {
             if (err) throw err;
             console.log("1 document inserted");
