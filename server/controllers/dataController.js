3ef3dddb3172ac094e3874e8bc30cc3851d634c7
Saku Rautiainen <saku.rautiainen@iki.fi>
2018-04-08 20:51:29 +0300

    Working api calls for saving data, fetching data for network and fetching network names

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -26,7 +26,66 @@ exports.mesh_data_packet = function (req, res) {
     });
     console.log(JSON.stringify(req.body));
     res.send();
+};
 
+exports.mesh_networks = function (req, res) {
+    console.log(req.params);
+    const mesh_name = req.params.mesh_network_name;
+    const query = {hostname: mesh_name};
+    mongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db(jsonConfig.mongodb_database);
+        dbo.collection(
+            jsonConfig.mongodb_collection_mesh_networks).find().toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+            res.send(result);
+        });
+    });
+};
 
+exports.mesh_network_data = function (req, res) {
+    console.log(req.params);
+    mongoClient.connect(url, function (err, db) {
+        const mesh_name = req.params.mesh_network_name;
+        const query = {hostname: mesh_name};
+        if (err) throw err;
+        var dbo = db.db(jsonConfig.mongodb_database);
+        dbo.collection(
+            jsonConfig.mongodb_collection_data).find(query).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+            res.send(result);
+        });
+    });
+};
 
+/* Doesnt work yet*/
+exports.register_mesh_network = function (req, res) {
+    mongoClient.connect(url, function (err, db) {
+        const mesh_name = req.params.mesh_network_name;
+        const query = {mesh_network_name: mesh_name};
+        if (err) throw err;
+        var dbo = db.db(jsonConfig.mongodb_database);
+        /* First lets check if it already has been registered*/
+        dbo.collection(
+            jsonConfig.mongodb_collection_data).find(query).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+            res.send(result);
+        });
+
+        /* If it has not been, lets register it*/
+        dbo.collection(
+            jsonConfig.mongodb_collection_data).insertOne(req.body, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+    console.log(JSON.stringify(req.body));
+    res.send();
 };
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,14 +3,12 @@ var router = express.Router();
 
 const data_controller = require("../controllers/dataController");
 
-router.get('/files', function(req, res, next) {
-  res.json([]);
-});
+router.get('/mesh_networks', data_controller.mesh_networks);
 
-router.get('/data', function(req, res, next) {
-  res.json([]);
-});
+router.get('/data/:mesh_network_name', data_controller.mesh_network_data);
 
 router.post('/data', data_controller.mesh_data_packet);
+
+router.post('/register_mesh_network/:mesh_network_name', data_controller.register_mesh_network);
 
 module.exports = router;
