916ea12382c5a74bea58f8cfb66b5aef0c0db94d
Saku Rautiainen <saku.rautiainen@iki.fi>
2023-04-05 22:29:37 +0300

    Renamed and moved files

---
+++ mesh_sensor_nodes/mesh_node.h
@@ -0,0 +1,15 @@
+//************************************************************
+// mesh sensor node header
+//
+
+//
+//
+//************************************************************
+
+enum meshNodeType {
+  MESH_GATEWAY = 1,
+  MESH_WEMOS_DHT12_NODE= 2,
+  MESH_DS18B20_NODE= 3,
+  MESH_WEMOS_SHT3X_NODE= 4,
+  MESH_UNKNOWN = 0
+};
--- mesh_sensor_nodes/mesh_node_wemos_sht3x/mesh_node_wemos_sht3x.ino
+++ mesh_sensor_nodes/mesh_node_wemos_sht3x/mesh_node_wemos_sht3x.ino
@@ -1,60 +1,49 @@
 //************************************************************
-// this is a simple example that uses the painlessMesh library
+// Sensor node with DS18B20 temperature sensor
 //
-// 1. sends a silly message to every node on the mesh at a random time betweew 1 and 5 seconds
-// 2. prints anything it recieves to Serial.print
+// 1. Reads temperature sensor
+// 2. Sends it to mesh network
 //
 //
 //************************************************************
 #include <painlessMesh.h>
-#include <painlessMeshSync.h>
-#include <painlessScheduler.h>
-#include <DHT.h>
-#include <DHT_U.h>
-
-#define DHTPIN 4     // what digital pin the DHT22 is conected to
-#define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define   MESH_PREFIX     "HakalaSensorNode"
 #define   MESH_PASSWORD   "password"
 #define   MESH_PORT       5555
 
-DHT dht(DHTPIN, DHTTYPE);
 
 enum meshNodeType {
   MESH_GATEWAY = 1,
   MESH_DHT_NODE= 2,
+  MESH_DS18B20_NODE= 3,
   MESH_UNKNOWN = 0
 };
-
-meshNodeType node_type = MESH_UNKNOWN;
 void sendMessage() ;
+meshNodeType node_type = MESH_DS18B20_NODE;
+Scheduler userScheduler;
 painlessMesh  mesh;
 Task taskSendMessage( TASK_SECOND * 600 , TASK_FOREVER, &sendMessage );
 
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;     
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
 void sendMessage() {
-    String error_msg = "";
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h = dht.readHumidity();
-    // Read temperature as Celsius (the default)
-    float t = dht.readTemperature();
-
-    // Check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t) ) {
-      error_msg = "Failed to read from sensor, nodeid: ";
-    }
-
-    
     String msg = ";";
-    if(error_msg.length() == 0) {
-      msg += mesh.getNodeId();
-      msg +=";H;" + String(h) + ";T;" + String(t) + ";\n";
-    }
-    else {
-      msg = mesh.getNodeId();
-      msg += error_msg;
-    }
+    sensors.requestTemperatures(); 
+    float temperatureC = sensors.getTempCByIndex(0);
+      
+    msg += mesh.getNodeId();
+    msg +=";H;NO_HUMIDITY;T;" + String(temperatureC) + ";\n";
+    
   
     mesh.sendBroadcast( msg );
     
@@ -62,11 +51,8 @@ void sendMessage() {
     taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
 }
 
-
-
-
 void receivedCallback( uint32_t from, String &msg ) {
-  Serial.printf(";R;%u;msg;%s", from, msg.c_str());
+  //Serial.printf(";R;%u;msg;%s", from, msg.c_str());
 }
 
 void newConnectionCallback(uint32_t nodeId) {
@@ -84,10 +70,10 @@ void nodeTimeAdjustedCallback(int32_t offset) {
 void setup() {
   Serial.begin(9600);
 
-//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
 
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
   mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 
 
@@ -96,9 +82,7 @@ void setup() {
   mesh.onNewConnection(&newConnectionCallback);
   mesh.onChangedConnections(&changedConnectionCallback);
 
-
-  node_type = MESH_DHT_NODE;
-  mesh.scheduler.addTask( taskSendMessage );
+  userScheduler.addTask( taskSendMessage );
   taskSendMessage.enable() ;
   Serial.printf("Setup Done");
 
